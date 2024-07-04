201b1fad5cd4c501266d7ce02e65c1d4ec82aa8c
Praveen Nagodagamage <praveen.nagodagamage@swisslog.com>
2024-07-04 21:19:13 +0200

    added background

--- app/view/[filename].tsx
+++ app/view/[filename].tsx
@@ -1,5 +1,12 @@
 import { Stack, Link, useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import {
+  ImageBackground,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ViewNote = () => {
   const { filename } = useLocalSearchParams();
@@ -7,18 +14,40 @@ const ViewNote = () => {
     <>
       <Stack.Screen options={{ headerShown: false }} />
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>View {filename}</Text>
-        <Link
-          href={{
-            pathname: "/edit/[filename]",
-            params: { filename: filename },
-          }}
+        <ImageBackground
+          source={require("../../assets/images/bg_view.png")}
+          style={{ width: "100%", height: "100%" }}
+          imageStyle={{ resizeMode: "repeat" }}
         >
-          Edit
-        </Link>
+          <Text>View {filename}</Text>
+          <Link
+            href={{
+              pathname: "/edit/[filename]",
+              params: { filename: filename },
+            }}
+          >
+            Edit
+          </Link>
+        </ImageBackground>
       </View>
     </>
   );
 };
 
 export default ViewNote;
+
+const styles = StyleSheet.create({
+  bgImage: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: 50,
+    height: 50,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 2,
+    padding: 10,
+  },
+});
