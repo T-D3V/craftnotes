95d56a886e1fb949cbdf6b3ffcf960a65c44b138
NJSchueni <noah.schaerer@icloud.com>
2024-07-04 17:00:04 +0200

    working background

--- app/edit/[filename].tsx
+++ app/edit/[filename].tsx
@@ -1,5 +1,8 @@
+import React from "react";
 import { Stack, useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+const image = require("../../assets/images/bg_edit.png");
 
 const EditNote = () => {
   const { filename } = useLocalSearchParams();
@@ -7,10 +10,31 @@ const EditNote = () => {
     <>
       <Stack.Screen options={{ headerShown: false }} />
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Edit {filename}</Text>
+        <ImageBackground
+          source={image}
+          resizeMode="repeat"
+          style={styles.image}
+        >
+          <Text>Edit {filename}</Text>
+        </ImageBackground>
       </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    textAlign: "center",
+    backgroundColor: "#000000c0",
+  },
+});
 
 export default EditNote;
